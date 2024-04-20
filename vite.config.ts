f54d2876069dda5ee825fb4a784df20caf2b3a07
Francesco <francesco.209@gmail.com>
2024-04-20 12:15:01 +0200

    Remove test, spec and stories from dist

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,13 @@ import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: ["src"] })],
+  plugins: [
+    react(),
+    dts({
+      include: ["src"],
+      exclude: ["**/*.test.*", "**/*.spec.*", "**/*.stories.*"],
+    }),
+  ],
   build: {
     copyPublicDir: false,
     lib: {
